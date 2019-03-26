2817708a587fb340e84da24fd0e858c47c6219dc
Phil Dugliss <phillipdugliss@gmail.com>
2019-03-26 17:35:38 -0500

    2nd commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,23 @@
+    
+    for(i=0; )
+    
+    // Creates an array that lists out all of the options (Rock, Paper, or Scissors).
+    var computerChoices = ["a", "b", "c"];
+
+    // This function is run whenever the user presses a key.
+    document.onkeyup = function(event) {
+
+      // Determines which key was pressed.
+      var userGuess = event.key;
+
+      // Randomly chooses a choice from the options array. This is the Computer's guess.
+      var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+      // Alerts the key the user pressed (userGuess).
+      alert("User guess: " + userGuess);
+
+      // Alerts the Computer's guess.
+      alert("Computer guess: " + computerGuess);
+
+    };
+ 
